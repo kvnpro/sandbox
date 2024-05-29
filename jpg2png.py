7a912f9991ac3f85969ee438b8f6aaefc5d46720
kvnpro <kross005@regis.edu>
2024-05-29 15:51:24 -0600

    add tkinter display image

--- jpg2png.py
+++ jpg2png.py
@@ -1,6 +1,15 @@
-from PIL import Image
+from tkinter import *
+from PIL import Image, ImageTk
 
 # from tkinter import *
 
 img = Image.open("/Users/kevin/Desktop/Tower73-p14.jpg")
 img.save("/Users/kevin/Desktop/testing.png")
+
+root = Tk()
+canvas = Canvas(root, width=1000, height=1000)
+canvas.pack()
+tkimg = ImageTk.PhotoImage(Image.open("/Users/kevin/Desktop/testing.png"))
+canvas.create_image(20, 20, anchor=NW, image=tkimg)
+
+root.mainloop()
