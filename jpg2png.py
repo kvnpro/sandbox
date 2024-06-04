7e46341eea9f2e7c27df20da4f79452b4087fac0
kvnpro <kross005@regis.edu>
2024-06-04 10:12:52 -0600

    add reference comment for Image display

--- jpg2png.py
+++ jpg2png.py
@@ -9,6 +9,11 @@ img.save("/Users/kevin/Desktop/testing.png")
 root = Tk()
 canvas = Canvas(root, width=1000, height=1000)
 canvas.pack()
+
+#
+# https://www.c-sharpcorner.com/blogs/basics-for-displaying-image-in-tkinter-python
+#
+
 tkimg = ImageTk.PhotoImage(Image.open("/Users/kevin/Desktop/testing.png"))
 canvas.create_image(20, 20, anchor=NW, image=tkimg)
 
