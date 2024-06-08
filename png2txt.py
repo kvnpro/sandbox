587db8e551a3534256cce86923197a0065a85f35
kvnpro <kross005@regis.edu>
2024-06-08 08:51:36 -0600

    sample tuition excerpt from SMA handbook

--- png2txt.py
+++ png2txt.py
@@ -7,7 +7,9 @@ def extract_text_from_image(image):
     return text
 
 
-easy_text_path = "/Users/kevin/Desktop/testing.png"
+easy_text_path = (
+    "/Users/kevin/Desktop/SMA Parents Handbook with 75-76 Insert/Scan 29.png"
+)
 
 easy_img = cv2.imread(easy_text_path)
 
